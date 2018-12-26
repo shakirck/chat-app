5cfbeb0281cf6397dd3fec13d1f0f04f1f116279
shakirck <shakirck333#gmail.com>
2018-12-26 22:25:33 +0530

    added message funcitonality

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require ('socket.io')(http);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
-app.get('/',function(req,res){
-res.sendFile(__dirname+'/index.html');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection',function(socket){
-    // console.log (' a new user connected');
-    socket.on('chat message',function(msg){
-        console.log('message :' +msg);
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
     });
-});
+  });
 
-http.listen(3000,function(){
-    console.log ('listening on *:3000');
-
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
